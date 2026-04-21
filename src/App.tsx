4c532ad92dca661ae6c9bc7111ecea16a3d0317f
Pedro Andre Dos Santos <pedro.santos@havan.com.br>
2026-04-21 00:19:16 -0300

    Configure GitHub Pages deploy via Actions

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
+import { HashRouter, Routes, Route, Navigate } from 'react-router-dom';
 import { ProjectProvider } from './context/ProjectContext';
 import Dashboard from './components/Dashboard/Dashboard';
 import TimelinePage from './components/Timeline/TimelinePage';
@@ -7,13 +7,13 @@ import './App.css';
 function App() {
   return (
     <ProjectProvider>
-      <BrowserRouter>
+      <HashRouter>
         <Routes>
           <Route path="/" element={<Dashboard />} />
           <Route path="/timeline/:projectId" element={<TimelinePage />} />
           <Route path="*" element={<Navigate to="/" replace />} />
         </Routes>
-      </BrowserRouter>
+      </HashRouter>
     </ProjectProvider>
   );
 }
